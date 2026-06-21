22a43a7c2b3db877ac2bf07103fb373883d2492d
Koushik Dasika <koushikdasika@gmail.com>
2026-06-21 12:52:11 -0400

    added supervisor

--- lib/jido_sandbox.ex
+++ lib/jido_sandbox.ex
@@ -1,7 +1,17 @@
 defmodule JidoSandbox do
+  use Application
+
   @moduledoc """
   Documentation for `JidoSandbox`.
   """
+
+  def start(_type, _args) do
+    children = [
+      {Jido.Browser.Pool, name: :default, size: 2, headless: true, startup_timeout: 60_000}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: JidoSandbox.Supervisor)
+  end
 
   @doc """
   Hello world.
